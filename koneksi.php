a13a76babb3674afcbb4abc1947f67b14d46b3c0
Mochammad Khalish Mulyadi <73516671+Khalish-Mulyadi@users.noreply.github.com>
2021-11-05 20:59:43 +0700

    Create connection config

--- koneksi.php
+++ koneksi.php
@@ -1,3 +1,9 @@
-Halo sayang
-Test
-add new feature 1
+<?php
+
+$host="localhost"; //host server
+$user="root"; //user login
+$pass =""; //password
+$db = "salon-fifin"; //nama db
+$conn = mysqli_connect($host, $user, $pass, $db) or die ("Koneksi gagal");
+
+?>
