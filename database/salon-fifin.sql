58cf126a7ea4d250b8ee19a95e6976219edce245
unknown <khalishmulyadi21@gmail.com>
2022-01-04 16:23:06 +0700

    update db

--- database/salon-fifin.sql
+++ database/salon-fifin.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Nov 17, 2021 at 02:46 PM
+-- Generation Time: Jan 04, 2022 at 10:21 AM
 -- Server version: 10.4.14-MariaDB
 -- PHP Version: 7.4.10
 
@@ -24,6 +24,27 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `admin`
+--
+
+CREATE TABLE `admin` (
+  `id_admin` int(11) NOT NULL,
+  `nama_admin` varchar(20) NOT NULL,
+  `role` enum('super admin','admin','','') NOT NULL,
+  `username` varchar(10) NOT NULL,
+  `password` varchar(250) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `admin`
+--
+
+INSERT INTO `admin` (`id_admin`, `nama_admin`, `role`, `username`, `password`) VALUES
+(1, 'Sarah', 'super admin', 'admin', 'admin');
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `promo`
 --
 
@@ -34,6 +55,13 @@ CREATE TABLE `promo` (
   `posted_promo` date NOT NULL,
   `due_promo` date NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `promo`
+--
+
+INSERT INTO `promo` (`id_promo`, `nama_promo`, `gambar_promo`, `posted_promo`, `due_promo`) VALUES
+(1, 'promo 1 updated', '61c2f0dcd5ee7.png', '2021-12-31', '2022-01-01');
 
 -- --------------------------------------------------------
 
@@ -46,12 +74,30 @@ CREATE TABLE `reservasi` (
   `nama_pengunjung` varchar(50) NOT NULL,
   `layanan` varchar(20) NOT NULL,
   `kontak` varchar(30) NOT NULL,
-  `waktu_reservasi` date NOT NULL
+  `waktu_reservasi` date NOT NULL,
+  `status_reservasi` enum('approved','declined','submitted','') NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `reservasi`
+--
+
+INSERT INTO `reservasi` (`id_reservasi`, `nama_pengunjung`, `layanan`, `kontak`, `waktu_reservasi`, `status_reservasi`) VALUES
+(1, 'Sarah', 'potong rambut', '08123456789', '2021-12-31', 'approved'),
+(2, 'Mochammad Khalish Mulyadi', 'Hairstyle', '087734578879', '0000-00-00', 'declined'),
+(3, 'Jamal', 'Hairstyle', '081122334455', '2022-01-03', 'approved'),
+(4, 'Kaira', 'Hairstyle', '081285678334', '2022-01-04', 'declined'),
+(5, 'Mochammad Khalish Mulyadi', 'Hairstyle', '087734578879', '2022-01-07', 'submitted');
 
 --
 -- Indexes for dumped tables
 --
+
+--
+-- Indexes for table `admin`
+--
+ALTER TABLE `admin`
+  ADD PRIMARY KEY (`id_admin`);
 
 --
 -- Indexes for table `promo`
@@ -70,16 +116,22 @@ ALTER TABLE `reservasi`
 --
 
 --
+-- AUTO_INCREMENT for table `admin`
+--
+ALTER TABLE `admin`
+  MODIFY `id_admin` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
 -- AUTO_INCREMENT for table `promo`
 --
 ALTER TABLE `promo`
-  MODIFY `id_promo` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id_promo` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- AUTO_INCREMENT for table `reservasi`
 --
 ALTER TABLE `reservasi`
-  MODIFY `id_reservasi` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id_reservasi` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
